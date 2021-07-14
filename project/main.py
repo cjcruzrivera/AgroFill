7ef484b986ccef2d6a7410ab8f335dbe3379c3b4
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-07-14 17:22:44 -0500

    implementar calculos en partidor proporcional y limpiar db

--- project/main.py
+++ project/main.py
@@ -14,6 +14,10 @@ from .calculos.vertedero import (
     y2_DesnivelTrap,
     y2_Desnivelrec
 )
+from .calculos.partidor import (
+    calculo_partidor,
+    partidor_movil
+)
 
 main = Blueprint('main', __name__)
 
@@ -113,18 +117,23 @@ def vertedero_post():
     }
 
     if data['tipo'] == "rectangular":
-        valorTirante = parametrosRect(data['caudalDis'],data['rugosidad'],data['pendiente'],data['base'],data['alturaMuro'])
+        valorTirante = parametrosRect(
+            data['caudalDis'], data['rugosidad'], data['pendiente'], data['base'], data['alturaMuro'])
     else:
-        valorTirante=parametrosTrap(data['caudalDis'],data['rugosidad'],data['pendiente'],data['base'],data['talud'],data['alturaMuro'])
-    
-    valoresDisVertedero = DisVertedero(valorTirante[0],data['carga'], data['caudalDis'], data['base'], data['talud'])
-    valoresCotas= Cotas(data['cotaA'],data['carga'], valoresDisVertedero[0])
+        valorTirante = parametrosTrap(data['caudalDis'], data['rugosidad'],
+                                      data['pendiente'], data['base'], data['talud'], data['alturaMuro'])
 
-    if  data['tipo'] == "rectangular": 
-        valores_y2_Desnivel=y2_Desnivelrec(valoresDisVertedero[6], data['rugosidad'],data['pendiente'],data['base'],valorTirante[0],data['caudalDis'],data['carga'])
+    valoresDisVertedero = DisVertedero(
+        valorTirante[0], data['carga'], data['caudalDis'], data['base'], data['talud'])
+    valoresCotas = Cotas(data['cotaA'], data['carga'], valoresDisVertedero[0])
+
+    if data['tipo'] == "rectangular":
+        valores_y2_Desnivel = y2_Desnivelrec(
+            valoresDisVertedero[6], data['rugosidad'], data['pendiente'], data['base'], valorTirante[0], data['caudalDis'], data['carga'])
     else:
-        valores_y2_Desnivel= y2_DesnivelTrap(valoresDisVertedero[6], data['rugosidad'],data['pendiente'],data['base'],data['talud'],valorTirante[0],data['caudalDis'],data['carga'])
-    
+        valores_y2_Desnivel = y2_DesnivelTrap(
+            valoresDisVertedero[6], data['rugosidad'], data['pendiente'], data['base'], data['talud'], valorTirante[0], data['caudalDis'], data['carga'])
+
     resultados = {
         "tirante_normal": round(valorTirante[0], 3),
         "tirante_critico": round(valorTirante[4], 3),
@@ -132,21 +141,21 @@ def vertedero_post():
         "borde_libre": round(valorTirante[2], 3),
         "froude": round(valorTirante[3], 3),
         "energia": round(valores_y2_Desnivel[1], 3),
-        "angulo": round(valoresDisVertedero[5],3),
-        "altura": round(valoresDisVertedero[0],3),
-        "ancho": round(valoresDisVertedero[1],3),
+        "angulo": round(valoresDisVertedero[5], 3),
+        "altura": round(valoresDisVertedero[0], 3),
+        "ancho": round(valoresDisVertedero[1], 3),
         "tirante_final": round(valores_y2_Desnivel[0], 3),
-        "longitud_total": round(valoresDisVertedero[2],3),
-        "longitud_1": round(valoresDisVertedero[3],3),
-        "longitud_2": 0.4,#Fijo
+        "longitud_total": round(valoresDisVertedero[2], 3),
+        "longitud_1": round(valoresDisVertedero[3], 3),
+        "longitud_2": 0.4,  # Fijo
         "longitud_3": abs(valoresDisVertedero[4]),
         "desnivel": round(valores_y2_Desnivel[2], 3),
-        "caudal": round(valoresDisVertedero[6],3),
-        "velocidad_vertedero": round(valoresDisVertedero[7],3),
-        "cotaA": round(valoresCotas[0],3),
-        "cotaB": round(valoresCotas[1],3),
-        "cotaC": round(valoresCotas[2],3),
-        "cotaD": round(valoresCotas[3],3),
+        "caudal": round(valoresDisVertedero[6], 3),
+        "velocidad_vertedero": round(valoresDisVertedero[7], 3),
+        "cotaA": round(valoresCotas[0], 3),
+        "cotaB": round(valoresCotas[1], 3),
+        "cotaC": round(valoresCotas[2], 3),
+        "cotaD": round(valoresCotas[3], 3),
     }
 
     if current_user.is_authenticated:
@@ -174,53 +183,122 @@ def partidor_post():
         "tirante_entrante": request.form.get("tirante_entrante"),
         "pendiente": request.form.get("pendiente"),
         "rugosidad": request.form.get("rugosidad"),
-        "talud_entrante": request.form.get("talud_entrante"),
+        "talud_entrante": request.form.get("talud_entrante") or 0,
         "long_vertedero": request.form.get("base_entrante"),
-        "angulo_transicion": request.form.get("angulo_transicion"),
+        "angulo_transicion": request.form.get("angulo_transicion") or 0,
         "cota_a": request.form.get("cota_a"),
         "caudal_pasante": request.form.get("caudal_pasante"),
         "base_pasante": request.form.get("base_pasante"),
         "tirante_pasante": request.form.get("tirante_pasante"),
-        "talud_pasante": request.form.get("talud_pasante"),
+        "talud_pasante": request.form.get("talud_pasante") or 0,
         "cota_b": request.form.get("cota_b"),
         "caudal_saliente": request.form.get("caudal_saliente"),
         "base_saliente": request.form.get("base_saliente"),
         "tirante_saliente": request.form.get("tirante_saliente"),
-        "talud_saliente": request.form.get("talud_saliente"),
+        "talud_saliente": request.form.get("talud_saliente") or 0,
         "cota_c": request.form.get("cota_c"),
     }
     print(json.dumps(data, sort_keys=False, indent=4))
 
+    resultado_partidor = calculo_partidor(float(data['caudal_entrante']),
+                                          float(data['tirante_entrante']),
+                                          float(data['base_entrante']),
+                                          float(data['talud_entrante']),
+                                          float(data['long_vertedero']),
+                                          float(data['rugosidad']),
+                                          float(data['cota_a']),
+                                          float(data['pendiente']),
+                                          float(data['caudal_pasante']),
+                                          float(data['tirante_pasante']),
+                                          float(data['base_pasante']),
+                                          float(data['talud_pasante']),
+                                          float(data['caudal_saliente']),
+                                          float(data['tirante_saliente']),
+                                          float(data['base_saliente']),
+                                          float(data['talud_saliente']),
+                                          float(data['angulo_transicion']),
+                                          float(data['cota_b']),
+                                          float(data['cota_c']))
+
+    velocidad_trape = "No aplica"
+    energia_trape = "No aplica"
+    ycrt_trape = "No aplica"
+    ecrt_trape = "No aplica"
+    tirante_partidor = "No aplica"
+    long_transicion = "No aplica"
+    long_transicionp = "No aplica"
+    long_transicions = "No aplica"
+
+    if data["tipo"] == "trapezoidal":
+        velocidad_trape = resultado_partidor['vel_trape']
+        energia_trape = resultado_partidor['ener_trape']
+        ycrt_trape = resultado_partidor['tirante_crt']
+        ecrt_trape = resultado_partidor['ener_crt']
+        tirante_partidor = resultado_partidor['tirante_partidor']
+        long_transicion = resultado_partidor['long_transicion1']
+        long_transicionp = resultado_partidor['long_transpa']
+        long_transicions = resultado_partidor['long_transsa']
+
+    angulo = "No aplica"
+    altura_vert = ""
+    carga_part = ""
+    if data['tipoPart'] == "movil":
+        resultado_movil = partidor_movil(float(data['caudal_entrante']),
+                                          float(data['tirante_entrante']),
+                                          float(data['base_entrante']),
+                                          float(data['talud_entrante']),
+                                          float(data['long_vertedero']),
+                                          float(data['rugosidad']),
+                                          float(data['cota_a']),
+                                          float(data['pendiente']),
+                                          float(data['caudal_pasante']),
+                                          float(data['tirante_pasante']),
+                                          float(data['base_pasante']),
+                                          float(data['talud_pasante']),
+                                          float(data['caudal_saliente']),
+                                          float(data['tirante_saliente']),
+                                          float(data['base_saliente']),
+                                          float(data['talud_saliente']),
+                                          float(data['angulo_transicion']),
+                                          float(data['cota_b']),
+                                          float(data['cota_c']))
+        angulo = resultado_movil["angulo_final"]
+        carga_part = resultado_movil['carga_movil']
+    else:
+        carga_part = resultado_partidor['carga_hidrau']
+    
+    altura_vert = resultado_partidor['altura_verte']
     resultados = {
-        "velocidad_trape": 25,
-        "energia_trape": 25,
-        "ycrt_trape": 25,
-        "ecrt_trape": 25,
-        "tirante_partidor": 25,
-        "long_transicion": 25,
-        "long_transicionp": 25,
-        "long_transicions": 25,
-        "velocidad": 25,
-        "energia_especifica": 25,
-        "tirante_critico": 25,
-        "energia_critica": 25,
-        "froude": 25,
-        "altura_vertedero": 25,
-        "carga_hidraulica": 25,
-        "angulo_inclinacion": 25,
-        "ancho_vertedero": 25,
-        "long_vertedero": 25,
-        "Ancho_qpasante": 25,
-        "Ancho_qsaliente": 25,
-        "prof_colchon": 25,
-        "long_colchon": 25,
-        "carga_hidraulicap": 25,
-        "carga_hidraulicas": 25,
-        "cota_1": 25,
-        "cota_2": 25,
-        "cota_3": 25,
-        "cota_4": 25,
-        "cota_5": 25,
+        "velocidad_trape": velocidad_trape,
+        "energia_trape": energia_trape,
+        "ycrt_trape": ycrt_trape,
+        "ecrt_trape": ecrt_trape,
+        "tirante_partidor": tirante_partidor,
+        "long_transicion": long_transicion,
+        "long_transicionp": long_transicionp,
+        "long_transicions": long_transicions,
+        "velocidad": resultado_partidor['velocidad'],
+        "energia_especifica": resultado_partidor['energia_especifica'],
+        "tirante_critico": resultado_partidor['tirante_critico'],
+        "energia_critica": resultado_partidor['energia_critica'],
+        "froude": resultado_partidor['numero_froude'],
+        "altura_vertedero": altura_vert,
+        "carga_hidraulica": carga_part,
+        "angulo_inclinacion": angulo,
+        "longitud_lamina": resultado_partidor['long_hoja'],
+        "ancho_vertedero": resultado_partidor['ancho_verte'],
+        "long_vertedero_max": resultado_partidor['max_vertedero'],
+        "Ancho_qpasante": resultado_partidor['long_pasante'],
+        "Ancho_qsaliente": resultado_partidor['long_saliente'],
+        "prof_colchon": resultado_partidor.get('espesor','0'),
+        "long_colchon": resultado_partidor.get('long_colchon', '0'),
+        "carga_hidraulicap": resultado_partidor['carga_pasante'],
+        "carga_hidraulicas": resultado_partidor['carga_saliente'],
+        "cota_3": resultado_partidor.get('cota_3', data['cota_a']),
+        "cota_1": resultado_partidor['cota_1'],
+        "cota_2": resultado_partidor['cota_2'],
+        "cota_4": resultado_partidor['cota_4'],
+        "cota_5": resultado_partidor['cota_5'],
     }
 
     if current_user.is_authenticated:
